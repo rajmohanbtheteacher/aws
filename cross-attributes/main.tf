68aa39f24f5a467497bce9b0ea5114eff35d6645
rajmohanbtheteacher <learn@rajmohanbtheteacher.in>
2025-01-17 18:04:17 +0530

    commit

--- cross-attributes/main.tf
+++ cross-attributes/main.tf
@@ -1,32 +1,32 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "~> 5.0"
     }
   }
 }
 
-provider aws {
-	region = "us-east-1"
+provider "aws" {
+  region = "us-east-1"
 }
 
 resource "aws_eip" "teraform-gen-eip" {
-  domain           = "vpc"
-  
+  domain = "vpc"
+
 }
 
 # Fetch the Existing VPC by Name or ID
 data "aws_vpc" "existing" {
   filter {
-    name = "tag:Name"
-    values = ["ue1-prod-vpc"]  # Replace with your VPC name
+    name   = "tag:Name"
+    values = ["ue1-prod-vpc"] # Replace with your VPC name
   }
 }
 
 
 resource "aws_security_group" "cross-attr-sg" {
-  name = "Cross-attribute-demo-terraform"
+  name        = "Cross-attribute-demo-terraform"
   description = "Demo Security Group Creation Using Terraforms"
   vpc_id      = data.aws_vpc.existing.id
 
@@ -38,9 +38,9 @@ resource "aws_security_group" "cross-attr-sg" {
 # Define Ingress Rule for Web Service
 resource "aws_vpc_security_group_ingress_rule" "ingress_rule_secure_web" {
   security_group_id = aws_security_group.cross-attr-sg.id
-  cidr_ipv4 = "${aws_eip.teraform-gen-eip.public_ip}/32"
-  from_port = 443
-  to_port = 443
-  ip_protocol = 6
+  cidr_ipv4         = "${aws_eip.teraform-gen-eip.public_ip}/32"
+  from_port         = 443
+  to_port           = 443
+  ip_protocol       = 6
 
 }
