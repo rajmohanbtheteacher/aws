3f0d229cdc49bbf7e490ef27841fc701edba0f6e
rajmohanbtheteacher <learn@rajmohanbtheteacher.in>
2025-03-04 12:59:47 +0530

    datasource-1

--- Data-Sources/main.tf
+++ Data-Sources/main.tf
@@ -1,3 +1,8 @@
-data "aws_instances" "instance_data" {
-  
+data "aws_instance" "foo" {
+  instance_id = "i-0e44bade906a03d07"
+
+    filter {
+    name   = "tag:Name"
+    values = ["BastionHost-Demo"]
+  }
 }
